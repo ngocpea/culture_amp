a379a68aba701a71c47d94c0b3da79e77353411b
Becca <beckvo@gmail.com>
2017-09-24 22:11:39 +1000

    06. count ratings. all tests pass

--- surveyor/lib/surveyor/survey.rb
+++ surveyor/lib/surveyor/survey.rb
@@ -13,6 +13,22 @@ module Surveyor
       total_responses_answered = @responses.count { |response| response.answers.any? {|answer| answer.question == question } }
       return (( total_responses_answered.to_f / total_responses )* 100).round(2) 
     end
+
+    def ratings(question)
+      the_rating = {}
+      (1..5).each{ |rating_value| the_rating[rating_value] = 0 }   
+      
+      for response in @responses do
+        for answer in response.answers do 
+   	  if answer.question == question
+   	    the_rating[answer.value] += 1
+	  end
+	end
+       end         
+	the_rating
+
+    end
+
    
   end
 end
